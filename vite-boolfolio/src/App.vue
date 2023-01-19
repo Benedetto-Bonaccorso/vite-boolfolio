2fa5b2a4d7d4724d75219598fb0f1e124478d714
Benedetto-Bonaccorso <BenedettoBonaccorso00@gmail.com>
2023-01-19 17:40:02 +0100

    milestone-5

--- vite-boolfolio/src/App.vue
+++ vite-boolfolio/src/App.vue
@@ -2,17 +2,19 @@
 import axios from "axios"
 import { state } from './state.js'
 import BodyMain from './components/BodyMain.vue'
+import ProjectCard from "./components/ProjectCard.vue"
 
 export default {
     components: {
         BodyMain,
+        ProjectCard
     }
 }
 </script>
 
 <template>
     <section class="vue-home">
-        <BodyMain />
+        <ProjectCard />
     </section>
 </template>
 
---
+++ vite-boolfolio/src/components/ProjectCard.vue
@@ -0,0 +1,47 @@
+<script>
+
+import axios from "axios"
+import { state } from '../state.js';
+
+export default{
+        name: "ProjectCard",
+        data(){
+            return{
+                state
+            }
+        },
+        mounted() {
+                this.state.url = `http://127.0.0.1:8000/api/projects`
+
+                axios.get(this.state.url)
+
+                .then(response => {
+                    this.state.entries = response.data.results.data
+                    this.state.info = response.data.info
+                    console.log(response.data.results)
+                    console.log(this.state.entries)
+                })
+        }
+    }
+</script>
+
+<template>
+    <div class="container-fluid">
+        <div class="row">
+
+            <div class="col-4 g-4" v-for="(entry,i) in state.entries">
+                <div class="card">
+                    <div class="card text-center">
+                        <h3 class="">Title: {{ entry.title }}</h3>
+                        <h5>Author: {{ entry.author }}</h5>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<style lang="scss" scoped>
+
+</style>
+
